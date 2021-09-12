7633629003fc196ade60e7633b8af6c0bea06856
phrbmr <73762126+phrbmr@users.noreply.github.com>
2021-09-12 00:16:21 +0000

    Memory Game

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //card options
+  
+  //list of images from the cards and the top
+  //name and relative path (images/...)
   const cardArray = [
     {
       name: 'fries',
@@ -51,28 +53,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   ]
 
-  cardArray.sort(() => 0.5 - Math.random())
+  cardArray.sort(() => 0.5 - Math.random()) // randomize the card position
 
-  const grid = document.querySelector('.grid')
+  const grid = document.querySelector('.grid') //pickup element with class name grid from the html
   const resultDisplay = document.querySelector('#result')
-  let cardsChosen = []
-  let cardsChosenId = []
+  let cardsChosen = []     // empty array of cards chosen
+  let cardsChosenId = []   // empty array of cards chosen ID
   let cardsWon = []
 
   //create your board
   function createBoard() {
-    for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement('img')
-      card.setAttribute('src', 'images/blank.png')
+    for (let i = 0; i < cardArray.length; i++) {   //looping over the card array
+      const card = document.createElement('img')   //creating an image element for each card
+      card.setAttribute('src', 'images/blank.png') //link to the image
       card.setAttribute('data-id', i)
-      card.addEventListener('click', flipCard)
-      grid.appendChild(card)
+      card.addEventListener('click', flipCard) //if the card is clicked invoke flipCard function
+      grid.appendChild(card)  //putting the images in the grid using append
     }
   }
 
   //check for matches
   function checkForMatch() {
-    const cards = document.querySelectorAll('img')
+    const cards = document.querySelectorAll('img') // pick all images
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
     
@@ -81,34 +83,34 @@ document.addEventListener('DOMContentLoaded', () => {
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
       alert('You have clicked the same image!')
     }
-    else if (cardsChosen[0] === cardsChosen[1]) {
+    else if (cardsChosen[0] === cardsChosen[1]) { // check with the images match
       alert('You found a match')
-      cards[optionOneId].setAttribute('src', 'images/white.png')
-      cards[optionTwoId].setAttribute('src', 'images/white.png')
-      cards[optionOneId].removeEventListener('click', flipCard)
-      cards[optionTwoId].removeEventListener('click', flipCard)
+      cards[optionOneId].setAttribute('src', 'images/white.png') //change both to white
+      cards[optionTwoId].setAttribute('src', 'images/white.png') //change both to white
+      cards[optionOneId].removeEventListener('click', flipCard)  //remove click
+      cards[optionTwoId].removeEventListener('click', flipCard)  //remove click
       cardsWon.push(cardsChosen)
-    } else {
-      cards[optionOneId].setAttribute('src', 'images/blank.png')
-      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+    } else {                   // if it does not match
+      cards[optionOneId].setAttribute('src', 'images/blank.png') //flip back
+      cards[optionTwoId].setAttribute('src', 'images/blank.png') //flip back
       alert('Sorry, try again')
     }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
-    if  (cardsWon.length === cardArray.length/2) {
+    cardsChosen = []    // clear the array to pick again
+    cardsChosenId = []  // clear the array to pick again
+    resultDisplay.textContent = cardsWon.length //show the score based on the array of cards won
+    if  (cardsWon.length === cardArray.length/2) { // if all picked, alert the player
       resultDisplay.textContent = 'Congratulations! You found them all!'
     }
   }
 
   //flip your card
   function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length ===2) {
-      setTimeout(checkForMatch, 500)
+    let cardId = this.getAttribute('data-id')  // variable of card ID from above
+    cardsChosen.push(cardArray[cardId].name)   // push th cards from the card away based on ID
+    cardsChosenId.push(cardId)                 // push th cards from the card away based on ID
+    this.setAttribute('src', cardArray[cardId].img) // put card in the grid when chosen
+    if (cardsChosen.length ===2) {             // limits to 2 cards per play
+      setTimeout(checkForMatch, 500)           // check for match (time out to slow a little)
     }
   }
 
